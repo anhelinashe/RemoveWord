c5085066db59638f8d504821c26d24208fb5b3ce
shepe <shepe@DESKTOP-0ST686C>
2024-08-21 20:04:49 +0300

    Add project files.

---
+++ RemoveWord1/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+namespace removeword
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Console.WriteLine("Hello,user! Enter the text:");
+            string inputText = Console.ReadLine();
+
+
+            Console.WriteLine("Enter X:");
+            int x = int.Parse(Console.ReadLine());
+
+            string[] words = inputText.Split(' ');
+
+
+            if (x < words.Length)
+            {
+                for (int i = x; i < words.Length; i++)
+                {
+                    Console.Write(words[i] + " ");
+                }
+            }
+        }
+    }
+}
